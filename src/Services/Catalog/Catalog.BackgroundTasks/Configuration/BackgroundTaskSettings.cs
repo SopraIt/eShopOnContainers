a4d3dc9d46315673b12e50160be159f9c350a2b9
samuele-cozzi-soprasteria <samuele.cozzi@soprasteria.com>
2019-05-03 14:57:20 +0200

    fix elk and new catalog background task

---
+++ src/Services/Catalog/Catalog.BackgroundTasks/Configuration/BackgroundTaskSettings.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog.BackgroundTasks.Configuration
+{
+    public class BackgroundTaskSettings
+    {
+        public string ConnectionString { get; set; }
+
+        public string EventBusConnection { get; set; }
+
+        public int GracePeriodTime { get; set; }
+
+        public int CheckUpdateTime { get; set; }
+
+        public string ProductSourceBaseUrl { get; set; }
+        public string SearchBaseUrl { get; set; }
+    }
+}
