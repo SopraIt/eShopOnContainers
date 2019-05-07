6511e0e341883900cbd461fc7e86ba9a3cf83424
samuele-cozzi-soprasteria <samuele.cozzi@soprasteria.com>
2019-05-07 12:23:58 +0200

    change azure pipeline

--- k8s/helm/login-k8s.ps1
+++ k8s/helm/login-k8s.ps1
@@ -6,8 +6,7 @@ param
 Write-Output "Requested By: $env:RELEASE_REQUESTEDFOREMAIL"
 
 Write-Output "Log into Azure use cluster admin service principal"
-az login --service-principal -u "$env:APP_SP_ID" `
-  -p "$servicePrincipalSecret" -t "$env:TENANT_ID"
+az login --service-principal -u "$env:APP_SP_ID" -p "$servicePrincipalSecret" -t "$env:TENANT_ID"
 
 Write-Output "Set the default Azure subscription"
 az account set --subscription "$env:SUBSCRIPTION_NAME"
@@ -28,8 +27,7 @@ Write-Output "Service Principal Name or ID: $USER_NAME"
 $KUBE_CONFIG_PATH = "./.kube/config"
 
 Write-Output "Get credentials and set up for kubectl to use"
-az aks get-credentials -g "$env:AKS_RG_NAME" `
-  -n "$env:AKS_CLUSTER_NAME" -a -f "$KUBE_CONFIG_PATH"
+az aks get-credentials -g "$env:AKS_RG_NAME" -n "$env:AKS_CLUSTER_NAME" -a -f "$KUBE_CONFIG_PATH"
 
 Write-Output "Get kubectl version info"
 kubectl --kubeconfig "$KUBE_CONFIG_PATH" version short
