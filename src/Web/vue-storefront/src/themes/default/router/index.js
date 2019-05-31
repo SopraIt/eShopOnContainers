908927bddb2fd1778a2fcdc76c49fb19de170a05
samuele-cozzi-soprasteria <samuele.cozzi@soprasteria.com>
2019-05-31 23:43:55 +0200

    test vue

--- src/Web/vue-storefront/src/themes/default/router/index.js
+++ src/Web/vue-storefront/src/themes/default/router/index.js
@@ -41,7 +41,9 @@ let routes = [
   { name: 'error', path: '/error', component: ErrorPage, meta: { layout: 'minimal' } },
   { name: 'custom-cms-page', path: '/custom-cms-page', component: CustomCmsPage },
   { name: 'cms-block-demo-page-ssr', path: '/cms-block-demo-page-ssr', component: CmsBlockDemoPageSsr },
-  { name: 'cms-page-sync', path: '/cms-page-sync', component: CmsData, props: {identifier: 'about-us', type: 'Page', sync: true} }
+  { name: 'cms-page-sync', path: '/cms-page-sync', component: CmsData, props: {identifier: 'about-us', type: 'Page', sync: true} },
+  { name: 'hc', path: '/hc', component: Static, props: {page: 'lorem', title: 'hc'}, meta: {title: 'hc', description: 'hc'} },
+  { name: 'liveness', path: '/liveness', component: Static, props: {page: 'lorem', title: 'liveness'}, meta: {title: 'liveness', description: 'liveness'} },
 ]
 if (!config.products.useShortCatalogUrls) {
   routes = routes.concat([
