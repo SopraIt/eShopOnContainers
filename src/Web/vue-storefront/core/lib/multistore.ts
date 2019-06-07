ac0dc8d37ccf8b3a6ce98de6408f8bb6a60edc03
samuele-cozzi-soprasteria <samuele.cozzi@soprasteria.com>
2019-06-07 22:51:34 +0200

    vue - manage stringify

--- src/Web/vue-storefront/core/lib/multistore.ts
+++ src/Web/vue-storefront/core/lib/multistore.ts
@@ -134,7 +134,7 @@ export function localizedDispatcherRoute (routeObj: LocalizedRoute | string, sto
   } 
   if (routeObj && routeObj.fullPath) { // case of using dispatcher
     const routeCodePrefix = rootStore.state.config.defaultStoreCode !== storeCode ? `/${storeCode}` : ''
-    const qrStr = "" // queryString.stringify(routeObj.params)
+    const qrStr = queryString.stringify(routeObj.params)
     return `${routeCodePrefix}/${routeObj.fullPath}${qrStr ? `?${qrStr}` : ''}`
   }
   return routeObj
